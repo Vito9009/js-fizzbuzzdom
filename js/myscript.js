ad90365509f3adf629ee219b2dd98c783f51b7ec
Vito9009 <yan.vi193@gmail.com>
2021-12-10 16:01:57 +0100

    css-e-struttura-base-javascript

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,14 @@
-for(let i = 1; i <= 100; i++){
-    console.log('numeroStampato', i);
+const container = document.getElementById("container"); //richiamo il container (dove inserirò i vari box) nel file HTML
+
+for(let i = 1; i <= 100; i++){ // creazione ciclo For
+
+    if((i % 3 == 0) && (i % 5 == 0)){
+        container.innerHTML += '<div class="box fizzbuzz">' + 'dfsf' + '</div>';
+    } else if(i % 5 == 0){
+        container.innerHTML += '<div class="box buzz">' + i + '</div>';
+    } else if(i % 3 == 0){
+        container.innerHTML += '<div class="box fizz">' + i + '</div>';
+    } else{
+        container.innerHTML += '<div class="box">' + i + '</div>';
+    }
 }
